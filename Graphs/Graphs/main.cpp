262a0a7713d3daf2aeb7b633688b312a51e2a8d5
YonatanReisinger <yonatanrisinger33@gmail.com>
2024-05-13 00:45:56 +0300

    added copy ctor, assignment operator, destructor and started the function that returns G transpose.

--- Graphs/Graphs/main.cpp
+++ Graphs/Graphs/main.cpp
@@ -6,7 +6,20 @@
 
 int main()
 {
-	Graph g(5);
-	Graph:: Vertex v(3);
-	cout << "hello" << endl;
+	Graph g(5), g2(5);
+	Graph::Vertex v1(1);
+	Graph::Vertex v2(2);
+	Graph:: Vertex v3(3);
+	Graph::Vertex v4(4);
+	Graph::Vertex v5(5);
+
+	g.AddEdge(v2, v3);
+	g.AddEdge(v2, v4);
+	g.AddEdge(v1, v2);
+	g.AddEdge(v4, v3);
+	g.AddEdge(v3, v5);
+
+	g2 = g.MakeTranspose();
+	cout << g2 << endl;
+
 }
